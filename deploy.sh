37e7606dc2c3ee97c4bde6972b4b33cc386dc66c
Jafish <526800921@qq.com>
2020-04-03 17:07:25 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:a526800921/a526800921.github.io.git master
 
 cd -
